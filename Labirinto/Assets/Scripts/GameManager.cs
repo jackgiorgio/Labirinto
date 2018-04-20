2c63317b9d406ead11049a2ac305623664687f95
jackgiorgio <418682632@qq.com>
2018-04-20 18:39:18 +0800

    Labirinto-Version1.0

---
+++ Labirinto/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public Maze mazePrefab;
+    public int sizeX, sizeZ;
+
+    private Maze mazeInstance;
+    private MazeCell[,] cells;
+    
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
